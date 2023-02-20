8d9e37446833ead61fbf926f8d33b9218aa51450
wastijeet110@gmail.com <ClinkRatCheat>
2023-02-21 00:08:43 +0530

    First change of the day

---
+++ 1st.py
@@ -0,0 +1,13 @@
+n=int(input("Enter your number of elements you want to enter:-"))
+
+s=[]
+
+
+for i in range(0,n):
+    a=input("Enter your elements:-")
+    s.append(a)
+
+
+print("Your stack contains:-",end="")
+for i in range(0,len(s)):
+    print(s.pop(),end="")
